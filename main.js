93cba22505b38224c2c1c677d8d20f25b5e496d0
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-06-30 15:59:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+Webcam.set({
+    height:300,
+    width:300,
+    image_format:"png",
+    png_quality: 100
+});
 
 
+Webcam.attach("#camera");
+
+function takeImage(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='image' src='"+data_uri+"'>"
+    });
+
+}
+
+console.log("version - ",ml5.version)
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/oMD21kWOJ/model.json", doneLoading);
+
+function doneLoading(){
+    console.log("done Loading")
+}
+
+function checkImage(){
+    img= document.getElementById("image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        document.getElementById("object").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
    
